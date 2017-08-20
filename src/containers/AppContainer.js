9850af050adc3f051fd5333f5e56e1d9b76347a0
dudulaopo833 <1294318974@qq.com>
2017-08-21 00:45:52 +0800

    Split AppContainer and AppComponnet

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,42 +1,6 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addTodoAction, toggleTodoAction, setFilterAction, visibilityFilters } from '../actions';
-import AddTodoComponent from '../components/AddTodoComponent';
-import TodoListComponent from '../components/TodoListComponent';
-import FooterComponent from '../components/FooterComponent';
-
-class AppComponent extends Component{
-	render(){
-		const { visibleTodos, visibleFilter, dispatchAddTodo, dispatchToggleTodo, dipatchFilterTodo} = this.props;
-
-		return (
-			<div>
-				<AddTodoComponent dispatchAddTodoAction={dispatchAddTodo} />
-				<TodoListComponent dispatchToggleTodoAction={dispatchToggleTodo} visibleTodos={visibleTodos} />
-				<FooterComponent dispatchFilterTodoAction={dipatchFilterTodo}  visibleFilter={visibleFilter}/>
-			</div>
-		);
-	}
-};
-
-AppComponent.propTypes = {
-	visibleTodos: PropTypes.arrayOf(PropTypes.shape({
-		text: PropTypes.string.isRequired,
-		completed: PropTypes.bool.isRequired,
-		id: PropTypes.number.isRequired
-	}).isRequired).isRequired,
-	visibleFilter: PropTypes.oneOf([
-	    'SHOW_ALL',
-	    'SHOW_COMPLETED',
-	    'SHOW_ACTIVE'
-	]).isRequired,
-	dispatchAddTodo: PropTypes.func.isRequired,
-	dispatchToggleTodo: PropTypes.func.isRequired,
-	dipatchFilterTodo: PropTypes.func.isRequired
-};
-
-// Below is the container
+import AppComponent from '../components/AppComponent';
 
 const getVisibleTodos = (todos, filter) => {
 	switch(filter) {
